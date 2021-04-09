c500b3844d9ef3293c35ae63a56544eb1be53435
shivam sharma <shivam.cloud02@gmail.com>
2021-04-09 09:32:53 +0530

    Resolved: Null value crash

--- app/src/main/java/com/example/unconvert/MainActivity.java
+++ app/src/main/java/com/example/unconvert/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.unconvert;
+ package com.example.unconvert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,15 +20,26 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Thanks for using UnConvert", Toast.LENGTH_SHORT).show();
-                String s = editText.getText().toString();
-                int kg = Integer.parseInt(s);
-                double pound = 2.205*kg;
-                textView.setText("The corresponding value in pound is "+pound);
-            }
-        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Thanks for using UnConvert", Toast.LENGTH_SHORT).show();
+//                String s = editText.getText().toString();
+//                int kg = Integer.parseInt(s);
+//                double pound = 2.205*kg;
+//                textView.setText("The corresponding value in pound is "+pound);
+//            }
+//        });
+    }
+    public void calculate(View view){
+        if(editText.getText().toString().length()>0){
+            String s = editText.getText().toString();
+            int kg = Integer.parseInt(s);
+            double pound = 2.205*kg;
+            Toast.makeText(MainActivity.this, "Thanks for using UnConvert", Toast.LENGTH_SHORT).show();
+            textView.setText("The corresponding value in pound is "+pound);
+        }else {
+            Toast.makeText(MainActivity.this, "Please enter some value!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
